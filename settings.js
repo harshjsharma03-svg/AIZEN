636fa908af99bf9388a3189f6cce67cdf6ff56e7
Harsh <harsh.jsharma0304@gmail.com>
2025-09-22 18:39:14 +0530

    first commit

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'OPTIMUS_PRIME',
+  author: 'KING',
+  botName: "EMILIA",
+  botOwner: 'AIZEN',
+  ownerNumber: '919371197380', 
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.8",
+};
+
+module.exports = settings;
